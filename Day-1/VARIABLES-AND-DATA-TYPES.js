cb95b59f28610da6ac991ce5108e192128c0ace5
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-07-13 23:42:16 +0530

    Day 1: Variables and data Types ,Thank You To Hitesh Choudhary

---
+++ Day-1/VARIABLES-AND-DATA-TYPES.js
@@ -0,0 +1,51 @@
+// Day 1: Variables and data Types
+
+// Tasks/Activities
+
+// Activity 1 : Variable Declaration
+
+// Task 1
+var chai;
+
+chai = 5;
+
+console.log(chai);
+
+// Task 2
+let tea;
+
+tea = 5;
+
+console.log(tea);
+
+// Activity 2 : Constant Declaration
+// Task 3
+const kadakChai = true;
+console.log(kadakChai);
+
+// Activity 3 : Data Types
+// Task 4
+let chai1 = 5;
+console.log(typeof chai1);
+let chai2 = "5";
+console.log(typeof chai2);
+let chai3 = true;
+console.log(typeof chai3);
+let chai4 = { chai: 5 };
+console.log(typeof chai4);
+let chai5 = ["Blacktea", "ReadTea", "GreenTea"];
+console.log(typeof chai5);
+
+// Activity 4
+// Task 5
+let chai6 = 6;
+console.log(chai6);
+chai6 = "6";
+console.log(chai6);
+
+// Activity 4
+// Task 6
+const chai7 = 7;
+console.log(chai7);
+chai7 = "7";
+console.log(chai7);
