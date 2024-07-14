19b0414e29ccedc585e046b3c70e29134779bc6f
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-07-14 22:50:04 +0530

    Day 2: Operators Thanks To Hitesh Choudhary Sir

---
+++ Day-2/Operators.js
@@ -0,0 +1,64 @@
+// Activity 1: Arithmetic Operations
+
+// Task 1:
+let chai1 = 5,
+  chai2 = 5;
+console.log(chai1 + chai2);
+
+// Task 2:
+let chai3 = 5,
+  chai4 = 5;
+console.log(chai3 - chai4);
+
+// Task 3:
+let chai5 = 5,
+  chai6 = 5;
+console.log(chai5 * chai6);
+
+// Task 4:
+let chai7 = 5,
+  chai8 = 5;
+console.log(chai7 / chai8);
+
+// Task1:
+let chai9 = 5,
+  chai10 = 5;
+console.log(chai9 % chai10);
+
+// Activity 2: Assignment Operators
+// Task 6: Use of +=
+let chai11 = 5;
+let gst = 0.9;
+chai11 += gst
+console.log(chai11)
+// Task 7: Use of -=
+let chai12 = 5;
+let discount = 0.9;
+chai12 -= discount
+console.log(chai12)
+
+// Activity 3: Comparision Operators
+// Task 8: Use of > and <
+let chai13 = 5;
+let chai14 = 6;
+console.log(chai13 > chai14)
+console.log(chai13 < chai14)
+// Task 9: Use of >= and <=
+console.log(chai13 >= chai14)
+// Task 10: Use of == and ===
+console.log(chai13 == chai14)
+console.log(chai13 === "5")
+console.log(chai13 === 5)
+
+// Activity 4: Logical Operators
+// Task 11: Uses the &&
+console.log(chai13 > 5 && chai14 <5)
+// Task 12: Uses the ||
+console.log(chai13 > 5 || chai14 <5)
+// Task 13: Uses the !
+console.log(!(chai13 > 5))
+
+// Activity 5 : Ternary Operator
+// Task14 : Check a number is +ve or -ve and log the result
+const chai15 = 15;
+console.log(chai15 > 0 ? "Positive" : "Negative");
